bb7d978dff6c34bcb5871561486a8dc511ffb673
enlore <n.e.lorenson@gmail.com>
2013-06-05 22:23:12 -0500

    Convert to unicode

--- ahlure.py
+++ ahlure.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 
-ERRLOG = '/tmp/ahlure.err.log'
-ADMINS = ['oneofy@gmail.com']
-CONTACTS = ['oneofy@gmail.com']
+ERRLOG = u'/tmp/ahlure.err.log'
+ADMINS = [u'oneofy@gmail.com']
+CONTACTS = [u'oneofy@gmail.com']
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('AHLURE_SETTINGS', silent=True)
+app.config.from_envvar(u'AHLURE_SETTINGS', silent=True)
 
 # let's log stuff!
 if not app.debug:
@@ -15,21 +15,21 @@ if not app.debug:
     from logging import Formatter, FileHandler
 
     # for files!
-    fh = FileHandler(app.config['ERRLOG'])
-    fh.setFormatter(Formatter('''
+    fh = FileHandler(app.config[u'ERRLOG'])
+    fh.setFormatter(Formatter(u'''
     '%(asctime)s %(levelname)s: %(message)s '
         '[in %(pathname)s:%(lineno)d]'
         '''))
     app.logger.addHandler(fh)
 
     # for emails yes!
-    serv = '127.0.0.1'
-    who = 'ahlure@chilidog.rokitpowered.net'
+    serv = u'127.0.0.1'
+    who = u'ahlure@chilidog.rokitpowered.net'
     whom = ADMINS
-    subj = '[AHLURE TOTALLY BARFED]'
+    subj = u'[AHLURE TOTALLY BARFED]'
     mh = SMTPHandler(serv, who, whom, subj)
     mh.setLevel(logging.ERROR)
-    mh.setFormatter(Formatter('''
+    mh.setFormatter(Formatter(u'''
     OH JESUS
     Message type: %(levelname)s
     Location: %(pathname)s:%(lineno)d
@@ -50,33 +50,33 @@ def send_mail(msg, rcpt):
     login = None
     password = None
     mime_msg = msg
-    mime_msg['Subject'] = '[ CONTACT FORM @ ahlure.net ]'
+    mime_msg[u'Subject'] = u'[ CONTACT FORM @ ahlure.net ]'
 
-    smtp = smtplib.SMTP('localhost', 143)
-    smtp.sendmail('ahlure@chilidog.rokitpowered.net', rcpt, mime_msg.as_string())
+    smtp = smtplib.SMTP(u'localhost', 143)
+    smtp.sendmail(u'ahlure@chilidog.rokitpowered.net', rcpt, mime_msg.as_string())
     smtp.quit()
 
 # routes
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods = [u'GET', u'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == u'POST':
         form = request.form
 
-        rcpt = form['contact-email']
-        subj = '[ahlure.net CONTACT FORM]'
-        msg = """
+        rcpt = form[u'contact-email']
+        subj = u'[ahlure.net CONTACT FORM]'
+        msg = u"""
         Submission to ahlure.net:
         Name:  %s
         Phone: %s
         Message: 
         %s
-        """ % (form['contact-name'], form['contact-phone'], form['contact-blurb'])
+        """ % (form[u'contact-name'], form[u'contact-phone'], form[u'contact-blurb'])
 
         send_mail(msg,CONTACTS)
-        return render_template('index.html', msg=msg)
+        return render_template(u'index.html', msg=msg)
 
-    return render_template('index.html')
+    return render_template(u'index.html')
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     app.debug = True
     app.run()
