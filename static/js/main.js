b54f53d956f505b3d90021257c8fc3b9a4c9d6bc
enlore <n.e.lorenson@gmail.com>
2013-06-13 21:02:07 -0500

    Presentational js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
+    $('#section1-link').on('click', function() {
+        $('section.active').removeClass('active');
+        $('#wrapper > section').addClass('hidden');
+        $('#section1').addClass('active');
+        return false;
+    });
 
+    $('#section2-link').on('click', function() {
+        $('section.active').removeClass('active');
+        $('#wrapper > section').addClass('hidden');
+        $('#section2').addClass('active');
+        return false;
+    });
+
+    $('#section3-link').on('click', function() {
+        $('section.active').removeClass('active');
+        $('#wrapper > section').addClass('hidden');
+        $('#section3').addClass('active');
+        return false;
+    });
+});
