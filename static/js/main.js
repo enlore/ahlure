9d9087dc2c6eec4c7f7553ed73ff76d1a919d0b2
enlore <n.e.lorenson@gmail.com>
2013-06-17 20:05:09 -0500

    Color them tabs

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,24 @@ $(document).ready(function() {
     $('#section1-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section1').addClass('active');
+        $(this).parent().addClass('active blue');
     });
 
     $('#section2-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section2').addClass('active');
+        $(this).parent().addClass('active red');
     });
 
     $('#section3-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section3').addClass('active');
+        $(this).parent().addClass('active yellow');
+    });
+
+    $('#section4-link').on('click', function() {
+        $('.active').removeClass('active');
+        $('#section4').addClass('active');
+        $(this).parent().addClass('active green');
     });
 });
