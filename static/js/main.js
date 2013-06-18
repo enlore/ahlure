e556567784931fac6dbb6b7f9a1b7be620926343
enlore <n.e.lorenson@gmail.com>
2013-06-17 20:17:36 -0500

    Turn that border active color

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
     $('#section1-link').on('click', function() {
         $('.active').removeClass('active');
-        $('#section1').addClass('active');
+        $('#section1').addClass('active blue');
         $(this).parent().addClass('active blue');
     });
 
     $('#section2-link').on('click', function() {
         $('.active').removeClass('active');
-        $('#section2').addClass('active');
+        $('#section2').addClass('active red');
         $(this).parent().addClass('active red');
     });
 
     $('#section3-link').on('click', function() {
         $('.active').removeClass('active');
-        $('#section3').addClass('active');
+        $('#section3').addClass('active yellow');
         $(this).parent().addClass('active yellow');
     });
 
     $('#section4-link').on('click', function() {
         $('.active').removeClass('active');
-        $('#section4').addClass('active');
+        $('#section4').addClass('active green');
         $(this).parent().addClass('active green');
     });
 });
