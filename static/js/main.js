6ec99544c1f12f1dbc997a30e433b459b2295d7b
enlore <n.e.lorenson@gmail.com>
2013-06-19 20:17:01 -0500

    Prevent links from loading

--- static/js/main.js
+++ static/js/main.js
@@ -3,23 +3,27 @@ $(document).ready(function() {
         $('.active').removeClass('active');
         $('#section1').addClass('active blue');
         $(this).parent().addClass('active blue');
+        return false;
     });
 
     $('#section2-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section2').addClass('active red');
         $(this).parent().addClass('active red');
+        return false;
     });
 
     $('#section3-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section3').addClass('active yellow');
         $(this).parent().addClass('active yellow');
+        return false;
     });
 
     $('#section4-link').on('click', function() {
         $('.active').removeClass('active');
         $('#section4').addClass('active green');
         $(this).parent().addClass('active green');
+        return false;
     });
 });
