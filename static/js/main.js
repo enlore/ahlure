8b320509faa8b00e5837e972d6fea4a0226415a9
enlore <n.e.lorenson@gmail.com>
2013-06-23 23:07:14 -0500

    Set color styles  and classes on li and a

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready( function() {
     $('nav.tabs ul li a').on('click', function() {
+        $('nav.tabs ul li.active').removeClass('active');
         $('.content.inner.active').removeClass('active');
         var activeTab = $(this).attr('href');
+        $(this).parent().addClass('active');
         $(activeTab).addClass('active'); 
         return false;
     }); 
