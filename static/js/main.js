36c7e1d11c50674e4ae5b821040a7c7c8209b829
enlore <n.e.lorenson@gmail.com>
2013-06-23 22:38:43 -0500

    Add data colors

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,10 @@ $(document).ready( function() {
     $('nav.tabs ul li a').on('click', function() {
         $('.content.inner.active').removeClass('active');
         var activeTab = $(this).attr('href');
+        var color = $(this).data('color');
         $(activeTab).addClass('active').fadeIn(); 
         console.log(activeTab);
+        console.log(color);
         return false;
     }); 
 });
