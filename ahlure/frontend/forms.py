90a7d12d46e218c1bf435fbeba5f0f2fdf80f856
enlore <n.e.lorenson@gmail.com>
2013-07-12 22:17:49 -0500

    Set csrf field on form

--- ahlure/frontend/forms.py
+++ ahlure/frontend/forms.py
@@ -1,8 +1,12 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField 
+from flask.ext.wtf import Form
+from flask.ext.wtf import (TextField, TextAreaField, SubmitField, HiddenField)
+from flask.ext.wtf.html5 import EmailField
 from flask.ext.wtf import Required, Email
 
 class ContactForm(Form):
+    hidden_field    = HiddenField()
     name    = TextField(u'Name:', [Required()])
     email   = EmailField(u'Email:', [Required(), Email()])
     phone   = TextField(u'Phone:', [Required()])
-    message = TextAreadField(u'Message:', [Required()])
+    message = TextAreaField(u'Message:', [Required()])
+    submit  = SubmitField()
