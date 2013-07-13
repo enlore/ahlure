4a9fcb9d7a950850cbf8456b2a50f6e7bfab3f65
enlore <n.e.lorenson@gmail.com>
2013-07-12 23:00:32 -0500

    Clean up forms formatting

--- ahlure/frontend/forms.py
+++ ahlure/frontend/forms.py
@@ -1,12 +1,12 @@
-from flask.ext.wtf import Form
+from flask.ext.wtf import Form, ValidationError
 from flask.ext.wtf import (TextField, TextAreaField, SubmitField, HiddenField)
-from flask.ext.wtf.html5 import EmailField
 from flask.ext.wtf import Required, Email
+from flask.ext.wtf.html5 import EmailField
 
 class ContactForm(Form):
     hidden_field    = HiddenField()
-    name    = TextField(u'Name:', [Required()])
-    email   = EmailField(u'Email:', [Required(), Email()])
-    phone   = TextField(u'Phone:', [Required()])
-    message = TextAreaField(u'Message:', [Required()])
-    submit  = SubmitField()
+    name            = TextField(u'Name:', [Required()])
+    email           = EmailField(u'Email:', [Required(), Email()])
+    phone           = TextField(u'Phone:', [Required()])
+    message         = TextAreaField(u'Message:', [Required()])
+    submit          = SubmitField()
