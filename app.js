c1de8abd0e9ebc02f26da74360d30da907636ad8
enlore <n.e.lorenson@gmail.com>
2013-12-18 19:39:40 -0600

    Config for deployment

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+
+exports.start = function () {
+    app.listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+}
